3ad5e566b87c47665e4a7e627c27b0cc5acae604
Joao Rodrigues <joao.biribiu@gmail.com>
2020-04-25 17:45:31 -0300

    Definião de Entidades #1

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public ICollection <ItemPedido> ItemPedido { get; set; }
+    }
+}
