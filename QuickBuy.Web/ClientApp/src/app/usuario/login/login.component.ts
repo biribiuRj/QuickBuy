d74c61589d297a10b030accbf3b715e055eac674
Joao Rodrigues <joao.biribiu@gmail.com>
2020-05-01 17:34:28 -0300

    primeira versão de autenticação

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../modelo/Usuario";
+import { Router, Route } from "@angular/router"
 
 @Component({
   selector: "app-login",
@@ -10,15 +11,17 @@ export class LoginComponent {
   public usuario;
   public usuarioAutenticado: boolean;
 
+
   public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"]
 
-  constructor() {
+  constructor(private router: Router) {
   this.usuario = new Usuario();
   }
 
   entrar() {
     if (this.usuario.email == "joao.biribiu@gmail.com" && this.usuario.senha == "1234") {
-      this.usuarioAutenticado = true;
+      localStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate(['/']);
     }
     alert("O Email digitado foi: " + this.usuario.email + " e a senha foi : " + this.usuario.senha + this.usuarioAutenticado)
   }
