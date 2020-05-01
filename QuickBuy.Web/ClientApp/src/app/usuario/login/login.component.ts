9797a0295c6dcf33ae1336c47d3c322f19fdf05f
Joao Rodrigues <joao.biribiu@gmail.com>
2020-05-01 13:24:49 -0300

    Testes de *ngIf

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../modelo/Usuario";
 
 @Component({
   selector: "app-login",
-  templateUrl:"./login.component.html",
+  templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"/*, bla bla.css, bla*/]
 })
 export class LoginComponent {
-  public email = ""
+  public usuario;
+  public usuarioAutenticado: boolean;
+
+  public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"]
+
+  constructor() {
+  this.usuario = new Usuario();
+  }
+
+  entrar() {
+    if (this.usuario.email == "joao.biribiu@gmail.com" && this.usuario.senha == "1234") {
+      this.usuarioAutenticado = true;
+    }
+    alert("O Email digitado foi: " + this.usuario.email + " e a senha foi : " + this.usuario.senha + this.usuarioAutenticado)
+  }
+  public titulo = "Imagem padrão do Site";
+
 }
